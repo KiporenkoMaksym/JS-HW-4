4133c3e55c439c016217357d4574f0776aa3c278
KiporenkoMaksym <124812516+KiporenkoMaksym@users.noreply.github.com>
2023-03-03 00:22:04 +0200

    Add files via upload

---
+++ task 1.js
@@ -0,0 +1,7 @@
+// const numbers = [1, 25, 46, 2, 249, 52, 83]
+// let total = 0
+// for (let i = 0; i < numbers.length; i +=1) {
+//     console.log(numbers[i]);
+//     total += numbers[i]
+// }
+// console.log(`Total`, total);
---
+++ task 2.js
@@ -0,0 +1,10 @@
+// let smile = (prompt (`Який Ваш настрій`))
+// let mood
+
+// switch (smile) {
+//     case (`щасливий`): mood = `=)`; break
+//     case (`сумний`): mood = `=(`; break
+//     case (`злий`): mood = `:-[`; break
+//     default: alert (`Такого настрою немає`);
+// }
+// alert (mood)
